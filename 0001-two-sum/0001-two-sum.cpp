1028445f9bfa74e6b0fbc974cefd70e803e0201f
Ragib Hasan <124962194+ragibcs@users.noreply.github.com>
2026-01-08 20:42:46 +0600

    Implement twoSum function in 0001-two-sum.cpp

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int m=nums.size();
+        for(int i=0;i<m;i++){
+            for(int j=i+1;j<m;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
+            }
+            
+        }
+        return {};
+    }
+};
